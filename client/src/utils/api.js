130cacdf35ad5fb326ad6565797b998a415d1a96
Tobsyd <adeyeratobi@gmail.com>
2025-07-10 12:34:06 +0100

    updated backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: import.meta.env.VITE_SERVER_URL || 'http://localhost:5000' });
+const API = axios.create({ baseURL: import.meta.env.VITE_SERVER_URL || 'https://chat-socket-io-axtk.onrender.com' });
 API.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
